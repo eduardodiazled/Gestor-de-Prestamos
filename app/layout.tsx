4a8d9d4d83f7299f9dd333740773296c76e7b7f4
Luis Diaz <lueddios17@gmail.com>
2025-12-17 23:32:35 -0500

    Fix: Update Page Title to 'ZALDO | Fintech'

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "ZALDO",
+  title: "ZALDO | Fintech",
   description: "Soluciones Fintech",
   icons: {
     icon: '/icon.png',
